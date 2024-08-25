57723d5f52eff7841ef26b624abcf0d7000ad4c8
lumunor <lukik884@gmail.com>
2024-08-25 22:39:35 +0300

    addClient

---
+++ src/main/java/Windows/ClientGUI.java
@@ -0,0 +1,46 @@
+package Windows;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class ClientGUI extends JFrame {
+    private static final int WIDTH = 400;
+    private static final int HEIGHT = 300;
+
+    private final JTextArea log = new JTextArea();
+
+    private final JPanel panelTop = new JPanel(new GridLayout(2,3));
+    private final JTextField tfIPAddress = new JTextField("127.0.0.1");
+    private final JTextField tfPort = new JTextField("8189");
+    private final JTextField tfLogin = new JTextField("evgrniy-ivanovich");
+    private final JPasswordField tfPassword = new JPasswordField("123456");
+    private final JButton btnLogin = new JButton("Login");
+
+    private final JPanel panelBotton = new JPanel(new BorderLayout());
+    private final JTextField tfMessage = new JTextField();
+    private final JButton btnSend = new JButton("Send");
+
+    public ClientGUI(){
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setSize(WIDTH, HEIGHT);
+        setTitle("Чат клиента");
+
+        panelTop.add(tfIPAddress);
+        panelTop.add(tfPort);
+        panelTop.add(tfLogin);
+        panelTop.add(tfPassword);
+        panelTop.add(btnLogin);
+        add(panelTop,BorderLayout.NORTH);
+
+        panelBotton.add(tfMessage, BorderLayout.CENTER);
+        panelBotton.add(btnSend, BorderLayout.EAST);
+        add(panelBotton, BorderLayout.SOUTH);
+
+        log.setEditable(false);
+        JScrollPane scrollLog = new JScrollPane(log);
+        add(scrollLog);
+
+        setVisible(true);
+    }
+}
