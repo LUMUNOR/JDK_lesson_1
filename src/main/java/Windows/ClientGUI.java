076fab722e3320ab1d1e714625aef533de640662
lumunor <lukik884@gmail.com>
2024-08-27 09:42:12 +0300

    27.08.9:41

--- src/main/java/Windows/ClientGUI.java
+++ src/main/java/Windows/ClientGUI.java
@@ -2,18 +2,27 @@ package Windows;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class ClientGUI extends JFrame {
     private static final int WIDTH = 400;
     private static final int HEIGHT = 300;
+
+    private String ipAddress;
+    private String port;
+    private String login;
+    private String password;
+
+    private Boolean connect;
 
     private final JTextArea log = new JTextArea();
 
     private final JPanel panelTop = new JPanel(new GridLayout(2,3));
     private final JTextField tfIPAddress = new JTextField("127.0.0.1");
     private final JTextField tfPort = new JTextField("8189");
-    private final JTextField tfLogin = new JTextField("evgrniy-ivanovich");
-    private final JPasswordField tfPassword = new JPasswordField("123456");
+    private final JTextField tfLogin = new JTextField();
+    private final JPasswordField tfPassword = new JPasswordField();
     private final JButton btnLogin = new JButton("Login");
 
     private final JPanel panelBotton = new JPanel(new BorderLayout());
@@ -26,6 +35,29 @@ public class ClientGUI extends JFrame {
         setSize(WIDTH, HEIGHT);
         setTitle("Чат клиента");
         setLocationRelativeTo(serverWindow);
+        connect = false;
+
+        tfMessage.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                getTexInLog();
+            }
+        });
+
+        btnSend.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+               getTexInLog();
+            }
+        });
+
+        btnLogin.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                ipAddress = tfIPAddress.getText();
+                port = tfPort.getText();
+                login = tfLogin.getText();
+                password = tfPassword.getText();
+                connect = true;
+            }
+        });
 
         panelTop.add(tfIPAddress);
         panelTop.add(tfPort);
@@ -43,5 +75,12 @@ public class ClientGUI extends JFrame {
         add(scrollLog);
 
         setVisible(true);
+    }
+
+    private void getTexInLog(){
+        if (tfMessage.getText() != null && !tfMessage.getText().trim().isEmpty()) {
+            log.append(tfMessage.getText()+"\n");
+            tfMessage.setText("");
+        } else tfMessage.setText("");
     }
 }
